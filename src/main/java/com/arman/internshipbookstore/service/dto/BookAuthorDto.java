1e6a06ec7e4358361434636c895d6badcf5f66ac
Arman <melkonyan.arman.03@gmail.com>
2025-04-09 23:56:01 +0400

    Custom dtos

---
+++ src/main/java/com/arman/internshipbookstore/service/dto/BookAuthorDto.java
@@ -0,0 +1,15 @@
+package com.arman.internshipbookstore.service.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class BookAuthorDto {
+    private Long id;
+    private Long authorId;
+    private String bookId;
+    private String role;
+}
