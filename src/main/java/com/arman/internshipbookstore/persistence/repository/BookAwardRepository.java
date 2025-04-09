840af3c3084fc2d19406c3773280a3fd448bbdbc
Arman <melkonyan.arman.03@gmail.com>
2025-04-09 23:51:49 +0400

    Remove non-necessary classes

--- src/main/java/com/arman/internshipbookstore/persistence/repository/BookAwardRepository.java
+++
@@ -1,9 +0,0 @@
-package com.arman.internshipbookstore.persistence.repository;
-
-import com.arman.internshipbookstore.persistence.entity.BookAward;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookAwardRepository extends JpaRepository<BookAward, Long> {
-}
--- src/main/java/com/arman/internshipbookstore/persistence/repository/BookGenreRepository.java
+++
@@ -1,9 +0,0 @@
-package com.arman.internshipbookstore.persistence.repository;
-
-import com.arman.internshipbookstore.persistence.entity.BookGenre;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookGenreRepository extends JpaRepository<BookGenre, Long> {
-}
