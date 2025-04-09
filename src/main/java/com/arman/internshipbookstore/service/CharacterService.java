8fe3e3e83c5b5d5d8b674831da6d58358f96938f
Arman <melkonyan.arman.03@gmail.com>
2025-04-09 23:57:46 +0400

    Define Characters service

---
+++ src/main/java/com/arman/internshipbookstore/service/CharacterService.java
@@ -0,0 +1,32 @@
+package com.arman.internshipbookstore.service;
+
+import com.arman.internshipbookstore.persistence.entity.Characters;
+import com.arman.internshipbookstore.persistence.repository.CharacterRepository;
+import com.arman.internshipbookstore.service.dto.CharacterDto;
+import com.arman.internshipbookstore.service.mapper.CharacterMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CharacterService {
+    private final CharacterRepository characterRepository;
+    private final CharacterMapper characterMapper;
+
+    public CharacterDto getCharacterByName(String name){
+        Characters character = characterRepository.getCharactersByName(name);
+
+        return characterMapper.mapToDto(character);
+    }
+
+    public Characters save(CharacterDto characterDto){
+        Characters characters = characterRepository.getCharactersByName(characterDto.getName());
+        if(characters!=null) return characters;
+
+        characters = characterMapper.mapDtoToCharacter(characterDto);
+
+        Characters characters1 = characterRepository.save(characters);
+
+        return characters1;
+    }
+}
