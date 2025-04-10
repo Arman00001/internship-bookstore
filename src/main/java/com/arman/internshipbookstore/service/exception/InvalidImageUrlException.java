2ddb28bbf39c45fd30f20d6ce9d67c98508c0ee5
Arman <melkonyan.arman.03@gmail.com>
2025-04-11 01:05:10 +0400

    custom exception

---
+++ src/main/java/com/arman/internshipbookstore/service/exception/InvalidImageUrlException.java
@@ -0,0 +1,7 @@
+package com.arman.internshipbookstore.service.exception;
+
+public class InvalidImageUrlException extends RuntimeException {
+    public InvalidImageUrlException(String message) {
+        super(message);
+    }
+}
