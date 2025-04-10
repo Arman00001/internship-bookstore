a621b3013dd038bcdbf575a39b136738bd0eb6a8
Arman <melkonyan.arman.03@gmail.com>
2025-04-10 16:36:51 +0400

    Dto for modelAttribute for fetching data

---
+++ src/main/java/com/arman/internshipbookstore/service/dto/BookSearchCriteria.java
@@ -0,0 +1,18 @@
+package com.arman.internshipbookstore.service.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class BookSearchCriteria {
+    private String title;
+    private String author_name;
+    private String publisher;
+    private String isbn;
+    private List<String> genres;
+}
