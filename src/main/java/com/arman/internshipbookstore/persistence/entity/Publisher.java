5e4a90c16ae78910889fc916faae6797f4a6f36f
Arman <melkonyan.arman.03@gmail.com>
2025-04-16 18:13:38 +0400

    Edit id for batching, add adding method

--- src/main/java/com/arman/internshipbookstore/persistence/entity/Publisher.java
+++ src/main/java/com/arman/internshipbookstore/persistence/entity/Publisher.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,12 +15,22 @@ import java.util.List;
 public class Publisher {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "publisher_id_seq")
+    @SequenceGenerator(
+            name = "publisher_id_seq",
+            sequenceName = "publisher_id_seq",
+            allocationSize = 50
+    )
     private Long id;
 
     @Column(name = "name", nullable = false, unique = true)
     private String name;
 
     @OneToMany(mappedBy = "publisher", cascade = CascadeType.MERGE)
-    private List<Book> books;
+    private List<Book> books = new ArrayList<>();
+
+    public void addBook(Book book){
+        books.add(book);
+        book.setPublisher(this);
+    }
 }
