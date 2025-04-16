9e55c2faadc8361d3e9cf13b5dcdd3058e6f2d7c
Arman <melkonyan.arman.03@gmail.com>
2025-04-16 22:50:44 +0400

    Download images async

--- src/main/java/com/arman/internshipbookstore/InternshipBookstoreApplication.java
+++ src/main/java/com/arman/internshipbookstore/InternshipBookstoreApplication.java
@@ -2,7 +2,9 @@ package com.arman.internshipbookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication
 public class InternshipBookstoreApplication {
 
