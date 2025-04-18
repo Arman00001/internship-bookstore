6e517216e878055c10bb4b80a17a8af8a84e5086
Arman <melkonyan.arman.03@gmail.com>
2025-04-18 21:16:03 +0400

    Add image download call

--- src/main/java/com/arman/internshipbookstore/controller/CsvUploadController.java
+++ src/main/java/com/arman/internshipbookstore/controller/CsvUploadController.java
@@ -1,19 +1,22 @@
 package com.arman.internshipbookstore.controller;
 
+import com.arman.internshipbookstore.service.AsyncImageDownloaderService;
 import com.arman.internshipbookstore.service.CsvUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController
+@RestController("/csv")
 @RequiredArgsConstructor
 public class CsvUploadController {
     private final CsvUploadService csvUploadService;
+    private final AsyncImageDownloaderService asyncImageDownloaderService;
 
-    @PostMapping("/uploadCSV")
+    @PostMapping("/upload")
     @ResponseStatus(HttpStatus.CREATED)
     public void uploadCSV(@RequestParam("file") MultipartFile file){
         csvUploadService.uploadFile(file);
+        asyncImageDownloaderService.scheduleImageDownloads();
     }
 }
