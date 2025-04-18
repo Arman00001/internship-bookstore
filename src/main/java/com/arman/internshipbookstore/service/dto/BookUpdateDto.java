ca1322a033bfaeaeac223ac12672aef265ca86da
Arman <melkonyan.arman.03@gmail.com>
2025-04-18 21:05:13 +0400

    Create dto for updating book

---
+++ src/main/java/com/arman/internshipbookstore/service/dto/BookUpdateDto.java
@@ -0,0 +1,19 @@
+package com.arman.internshipbookstore.service.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class BookUpdateDto {
+    @NotNull(message = "Identifier should be given")
+    private Long id;
+    private String description;
+    private Integer starRating;
+    private String award;
+    private String character;
+
+}
