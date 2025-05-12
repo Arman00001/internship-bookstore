e9564b4b4ac1bb7e2d8cc826dbea10df03fae460
Arman <melkonyan.arman.03@gmail.com>
2025-05-12 14:53:03 +0400

    Create CustomUserDetailsService, implement "loadUserByUsername" method

---
+++ src/main/java/com/arman/internshipbookstore/security/CustomUserDetailsService.java
@@ -0,0 +1,45 @@
+package com.arman.internshipbookstore.security;
+
+import com.arman.internshipbookstore.persistence.entity.Role;
+import com.arman.internshipbookstore.persistence.entity.UserCredentials;
+import com.arman.internshipbookstore.persistence.entity.UserProfile;
+import com.arman.internshipbookstore.persistence.repository.UserCredentialRepository;
+import com.arman.internshipbookstore.persistence.repository.UserProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserCredentialRepository userCredentialRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserCredentials userCredentials = userCredentialRepository.findByUsername(username)
+                .orElseThrow(()->new BadCredentialsException("Bad credentials"));
+
+        UserProfile userProfile = userCredentials.getUserProfile();
+
+        if (!userProfile.isEnabled()) {
+            throw new LockedException("User is locked");
+        }
+
+        final Role role = userProfile.getRole();
+
+
+        return new org.springframework.security.core.userdetails.User(
+                userCredentials.getUsername(),
+                userCredentials.getPassword(),
+                List.of(new SimpleGrantedAuthority(role.getName().name()))
+        );
+    }
+}
