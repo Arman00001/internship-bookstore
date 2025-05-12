905c22dfee6cd1935978de02b94a70311523a369
Arman <melkonyan.arman.03@gmail.com>
2025-05-12 14:51:52 +0400

    Change RequestMapping address

--- src/main/java/com/arman/internshipbookstore/controller/CsvUploadController.java
+++ src/main/java/com/arman/internshipbookstore/controller/CsvUploadController.java
@@ -4,16 +4,18 @@ import com.arman.internshipbookstore.service.AsyncImageDownloaderService;
 import com.arman.internshipbookstore.service.CsvUploadService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@RestController("/csv")
+@RestController
+@RequestMapping("/api/csv")
 @RequiredArgsConstructor
 public class CsvUploadController {
     private final CsvUploadService csvUploadService;
     private final AsyncImageDownloaderService asyncImageDownloaderService;
 
-    @PostMapping("/upload")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void uploadCSV(@RequestParam("file") MultipartFile file){
         csvUploadService.uploadFile(file);
