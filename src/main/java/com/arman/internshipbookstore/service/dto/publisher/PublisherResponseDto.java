884e05182d24df8d5cd1c2426fed4ac43d6ed10b
Arman <melkonyan.arman.03@gmail.com>
2025-05-14 22:03:03 +0400

    Add constructor

--- src/main/java/com/arman/internshipbookstore/service/dto/publisher/PublisherResponseDto.java
+++ src/main/java/com/arman/internshipbookstore/service/dto/publisher/PublisherResponseDto.java
@@ -1,5 +1,6 @@
 package com.arman.internshipbookstore.service.dto.publisher;
 
+import com.arman.internshipbookstore.persistence.entity.Publisher;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,5 +16,14 @@ public class PublisherResponseDto {
     public PublisherResponseDto(Long id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public PublisherResponseDto(Publisher publisher) {
+        this.id = publisher.getId();
+        this.name = publisher.getName();
+    }
+
+    public static PublisherResponseDto getPublisherResponse(Publisher publisher) {
+        return new PublisherResponseDto(publisher);
     }
 }
