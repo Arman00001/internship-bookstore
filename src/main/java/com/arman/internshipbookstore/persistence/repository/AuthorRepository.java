79c437810c03561a3dfd974c0621edec8a277dd8
Arman <melkonyan.arman.03@gmail.com>
2025-05-15 03:06:22 +0400

    commit

--- src/main/java/com/arman/internshipbookstore/persistence/repository/AuthorRepository.java
+++ src/main/java/com/arman/internshipbookstore/persistence/repository/AuthorRepository.java
@@ -2,18 +2,13 @@ package com.arman.internshipbookstore.persistence.repository;
 
 import com.arman.internshipbookstore.persistence.entity.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.Set;
 
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
-    Author getAuthorByName(String name);
 
-    @Query("SELECT a.name FROM Author a")
-    Set<String> findAllNames();
+    Author getAuthorByName(String name);
 
     Author getAuthorById(Long id);
 }
