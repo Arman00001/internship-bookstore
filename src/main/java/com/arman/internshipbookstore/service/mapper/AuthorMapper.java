028e0722ae68607a8ace46d6e0a51b2e527da7b3
Arman <melkonyan.arman.03@gmail.com>
2025-05-15 03:09:24 +0400

    commit

--- src/main/java/com/arman/internshipbookstore/service/mapper/AuthorMapper.java
+++ src/main/java/com/arman/internshipbookstore/service/mapper/AuthorMapper.java
@@ -1,20 +1,22 @@
 package com.arman.internshipbookstore.service.mapper;
 
 import com.arman.internshipbookstore.persistence.entity.Author;
-import com.arman.internshipbookstore.service.dto.AuthorDto;
+import com.arman.internshipbookstore.service.dto.author.AuthorCreateDto;
+import com.arman.internshipbookstore.service.dto.author.AuthorDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthorMapper {
-    public Author mapDtoToAuthor(AuthorDto authorDto){
-        if(authorDto==null) return null;
+
+    public Author mapDtoToAuthor(AuthorCreateDto authorCreateDto) {
+        if (authorCreateDto == null) return null;
         Author author = new Author();
-        author.setName(authorDto.getName());
+        author.setName(authorCreateDto.getName());
         return author;
     }
 
-    public AuthorDto mapToDto(Author author){
-        if(author==null) return null;
+    public AuthorDto mapToDto(Author author) {
+        if (author == null) return null;
         AuthorDto authorDto = new AuthorDto();
         authorDto.setName(author.getName());
         authorDto.setId(author.getId());
