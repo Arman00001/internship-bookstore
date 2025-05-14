5fa8e99828f0361e43288e354c353cc702e96181
Arman <melkonyan.arman.03@gmail.com>
2025-05-14 22:02:48 +0400

    Add response dto

--- src/main/java/com/arman/internshipbookstore/service/dto/AuthorResponseDto.java
+++ src/main/java/com/arman/internshipbookstore/service/dto/AuthorResponseDto.java
@@ -1,4 +1,23 @@
 package com.arman.internshipbookstore.service.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class AuthorResponseDto {
+
+    private Long id;
+
+    private String name;
+
+    private String role;
+
+    public AuthorResponseDto(Long id, String name, String role) {
+        this.id = id;
+        this.name = name;
+        this.role = role;
+    }
 }
