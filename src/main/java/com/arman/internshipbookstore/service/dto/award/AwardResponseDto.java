20ae9d34caf17dae6f6d6c246e78dcfa7e450dec
Arman <melkonyan.arman.03@gmail.com>
2025-05-15 03:09:02 +0400

    Remove year field

--- src/main/java/com/arman/internshipbookstore/service/dto/award/AwardResponseDto.java
+++ src/main/java/com/arman/internshipbookstore/service/dto/award/AwardResponseDto.java
@@ -1,25 +1,18 @@
 package com.arman.internshipbookstore.service.dto.award;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class AwardResponseDto {
 
     private Long id;
 
     private String name;
 
-    private Integer year;
-
-    public AwardResponseDto(Long id,
-                            String name,
-                            Integer year){
-        this.id=id;
-        this.name=name;
-        this.year=year;
+    public AwardResponseDto(Long id, String name) {
+        this.id = id;
+        this.name = name;
     }
 }
