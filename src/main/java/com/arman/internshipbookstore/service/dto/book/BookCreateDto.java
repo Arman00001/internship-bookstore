d8a4f1024e88dee640c71105e422b4c3304eb9a7
Arman <melkonyan.arman.03@gmail.com>
2025-05-15 03:07:22 +0400

    Add dto

--- src/main/java/com/arman/internshipbookstore/service/dto/book/BookCreateDto.java
+++ src/main/java/com/arman/internshipbookstore/service/dto/book/BookCreateDto.java
@@ -1,4 +1,56 @@
 package com.arman.internshipbookstore.service.dto.book;
 
+import com.arman.internshipbookstore.controller.deserializer.StringToGenreSetDeserializer;
+import com.arman.internshipbookstore.enums.Genre;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Getter
+@Setter
 public class BookCreateDto {
+
+    @NotBlank(message = "Book title should not be empty")
+    private String title;
+
+    @NotBlank(message = "Book format should not be empty")
+    private String format;
+
+    @NotBlank(message = "Book publisher name should not be empty")
+    private Long publisherId;
+
+    @NotBlank(message = "Book language should not be empty")
+    private String language;
+
+    @NotBlank(message = "Book author names should not be empty")
+    private Map<String, List<String>> authorIdRoles;
+
+    @JsonDeserialize(using = StringToGenreSetDeserializer.class)
+    private Set<Genre> genres;
+
+    private String description;
+    private String series;
+    private Double rating;
+    private Integer pages;
+    private Long isbn;
+    private LocalDate publishDate;
+    private LocalDate firstPublishDate;
+    private String edition;
+    private Integer numRatings;
+    private Integer likedPercent;
+    private String setting;
+    private Integer bbeScore;
+    private Integer bbeVotes;
+    private Double price;
+    private String ratingsByStars;
+    private String awards;
+    private String imageUrl;
+    private String characters;
+
 }
