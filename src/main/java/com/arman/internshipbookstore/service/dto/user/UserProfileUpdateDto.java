8f9d8d13052239cc40baa4a4f1ffb8181ecce1b1
Arman <melkonyan.arman.03@gmail.com>
2025-05-15 22:48:44 +0400

    Rename class

--- src/main/java/com/arman/internshipbookstore/service/dto/user/UserProfileUpdateDto.java
+++ src/main/java/com/arman/internshipbookstore/service/dto/user/UserProfileUpdateDto.java
@@ -1,16 +1,18 @@
 package com.arman.internshipbookstore.service.dto.user;
 
+import com.arman.internshipbookstore.security.annotation.ValidPassword;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class UserUpdateDto {
+public class UserProfileUpdateDto {
 
     @NotBlank
     private String firstname;
 
     @NotBlank
     private String lastname;
+
 }
