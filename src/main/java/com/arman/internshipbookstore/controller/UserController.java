9ae5bdaea051962c5fa507c98ab07d889843abdb
Arman <melkonyan.arman.03@gmail.com>
2025-05-15 22:51:41 +0400

    Add userUpdate methods

--- src/main/java/com/arman/internshipbookstore/controller/UserController.java
+++ src/main/java/com/arman/internshipbookstore/controller/UserController.java
@@ -1,12 +1,12 @@
 package com.arman.internshipbookstore.controller;
 
 import com.arman.internshipbookstore.service.UserService;
-import com.arman.internshipbookstore.service.dto.user.UserDto;
-import com.arman.internshipbookstore.service.dto.user.UserUpdateDto;
+import com.arman.internshipbookstore.service.dto.user.*;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -29,9 +29,33 @@ public class UserController {
         return ResponseEntity.ok(userService.getById(id));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody @Valid UserUpdateDto userUpdateDto){
-        return ResponseEntity.ok(userService.updateUser(id,userUpdateDto));
+    @PutMapping("/{id}/profile")
+    public ResponseEntity<UserDto> updateUserProfile(@PathVariable Long id, @RequestBody @Valid UserProfileUpdateDto userProfileUpdateDto){
+        return ResponseEntity.ok(userService.updateUser(id, userProfileUpdateDto));
+    }
+
+    @PutMapping("/{id}/email")
+    public ResponseEntity<Void> updateUserEmail(@PathVariable("id") Long id,
+                                                UserEmailUpdateDto userEmailUpdateDto,
+                                                Authentication auth){
+        userService.updateUserEmail(id,userEmailUpdateDto,auth);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}/password")
+    public ResponseEntity<Void> updateUserPassword(@PathVariable("id") Long id,
+                                                   UserPasswordUpdateDto userPasswordUpdateDto,
+                                                   Authentication auth){
+        userService.updateUserPassword(id,userPasswordUpdateDto,auth);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}/role")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+    public ResponseEntity<Void> updateUserRole(@PathVariable("id") Long id,
+                                               UserRoleUpdateDto userRoleUpdateDto){
+        userService.updateUserRole(id,userRoleUpdateDto);
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("/{id}")
