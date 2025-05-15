27310a20b4209aa5b0d155698d9dfdef1ad1ccef
Arman <melkonyan.arman.03@gmail.com>
2025-05-15 22:47:08 +0400

    Remove ROLE_GUEST, it is the default anyway

--- src/main/java/com/arman/internshipbookstore/enums/RoleName.java
+++ src/main/java/com/arman/internshipbookstore/enums/RoleName.java
@@ -1,7 +1,6 @@
 package com.arman.internshipbookstore.enums;
 
 public enum RoleName {
-    ROLE_GUEST, // Can search/view books,authors,etc
     ROLE_USER, // Same as GUEST, plus leave ratings, reviews
     ROLE_USER_PRIME, //Can get chosen book for free for 1 month
     ROLE_MODERATOR, // Add/delete entities
