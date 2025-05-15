97d023b3a7379c5a0b7ab32ef733f8ec2cec1078
Arman <melkonyan.arman.03@gmail.com>
2025-05-16 03:40:00 +0400

    Add "exists" check

--- src/main/java/com/arman/internshipbookstore/persistence/repository/PublisherRepository.java
+++ src/main/java/com/arman/internshipbookstore/persistence/repository/PublisherRepository.java
@@ -1,12 +1,14 @@
 package com.arman.internshipbookstore.persistence.repository;
 
 import com.arman.internshipbookstore.persistence.entity.Publisher;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
+
     Publisher getPublisherByName(String name);
 
-    Publisher getPublisherById(Long id);
+    boolean existsPublisherByName(@NotBlank String name);
 }
