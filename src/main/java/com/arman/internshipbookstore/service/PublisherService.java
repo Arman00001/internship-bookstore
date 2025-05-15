d8b2400fdb9d9ba8a3cd93e1bfe81c696fd543f8
Arman <melkonyan.arman.03@gmail.com>
2025-05-16 03:39:41 +0400

    Add update method

--- src/main/java/com/arman/internshipbookstore/service/PublisherService.java
+++ src/main/java/com/arman/internshipbookstore/service/PublisherService.java
@@ -1,10 +1,15 @@
 package com.arman.internshipbookstore.service;
 
+import com.arman.internshipbookstore.persistence.entity.Book;
 import com.arman.internshipbookstore.persistence.entity.Publisher;
 import com.arman.internshipbookstore.persistence.repository.PublisherRepository;
-import com.arman.internshipbookstore.service.dto.publisher.PublisherDto;
+import com.arman.internshipbookstore.service.dto.publisher.PublisherCreateDto;
+import com.arman.internshipbookstore.service.dto.publisher.PublisherResponseDto;
+import com.arman.internshipbookstore.service.dto.publisher.PublisherUpdateDto;
 import com.arman.internshipbookstore.service.exception.PublisherNotFoundException;
+import com.arman.internshipbookstore.service.exception.ResourceAlreadyUsedException;
 import com.arman.internshipbookstore.service.mapper.PublisherMapper;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,57 +18,73 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class PublisherService {
-    private final PublisherMapper publisherMapper;
 
+    private final PublisherMapper publisherMapper;
     private final PublisherRepository publisherRepository;
 
-    public Publisher getPublisherById(Long id){
-        return publisherRepository.getPublisherById(id);
+    public PublisherResponseDto getPublisherById(Long id) {
+        Publisher publisher = publisherRepository.findById(id)
+                .orElseThrow(() ->
+                        new PublisherNotFoundException("Publisher with the following id not found: " + id));
+
+        return PublisherResponseDto.getPublisherResponse(publisher);
     }
 
-    public Publisher getPublisherByName(String name){
+    public PublisherResponseDto getPublisherByName(String name) {
         Publisher publisher = publisherRepository.getPublisherByName(name);
 
-        return publisher;
-    }
-
-    public Publisher save(PublisherDto publisherDto){
-        Publisher publisher = publisherRepository.getPublisherByName(publisherDto.getName());
-        if(publisher != null) return publisher;
-
-        publisher = publisherMapper.mapDtoToPublisher(publisherDto);
-
-        Publisher publisher1 = publisherRepository.save(publisher);
-
-        return publisher1;
+        return PublisherResponseDto.getPublisherResponse(publisher);
     }
 
     public List<Publisher> findAll() {
         return publisherRepository.findAll();
     }
 
-    public PublisherDto addPublisher(PublisherDto publisherDto) {
-        Publisher publisher = publisherRepository.getPublisherByName(publisherDto.getName());
+    public PublisherResponseDto addPublisher(PublisherCreateDto publisherCreateDto) {
+        Publisher pub = publisherRepository.getPublisherByName(publisherCreateDto.getName());
 
-        if(publisher==null){
-            publisher = publisherMapper.mapDtoToPublisher(publisherDto);
+        if (pub == null) {
+            pub = publisherMapper.mapDtoToPublisher(publisherCreateDto);
 
-            Publisher publisher1 = publisherRepository.save(publisher);
+            Publisher publisher = publisherRepository.save(pub);
 
-            return publisherMapper.mapToDto(publisher1);
+            return PublisherResponseDto.getPublisherResponse(publisher);
         }
 
-        return null;
+        throw new ResourceAlreadyUsedException("Publisher with following name already exists: " + publisherCreateDto.getName());
     }
 
     public void deletePublisher(Long id) {
-        Publisher publisher = publisherRepository.getPublisherById(id);
-        if(publisher==null)
-            throw new PublisherNotFoundException("Publisher with the following id does not exist: "+id);
+        Publisher publisher = publisherRepository.findById(id)
+                .orElseThrow(() ->
+                        new PublisherNotFoundException("Publisher with the following id does not exist: " + id));
 
-        if(!publisher.getBooks().isEmpty())
+        if (!publisher.getBooks().isEmpty()) {
             throw new IllegalStateException("Cannot delete publisher: it still has books associated.");
+        }
 
         publisherRepository.delete(publisher);
+    }
+
+    public PublisherResponseDto updatePublisher(Long id, @Valid PublisherUpdateDto publisherUpdateDto) {
+        Publisher publisher = publisherRepository.findById(id)
+                .orElseThrow(() ->
+                        new PublisherNotFoundException("Publisher with the following id does not exist: " + id));
+
+        if(publisherRepository.existsPublisherByName(publisherUpdateDto.getName())) {
+            throw new IllegalArgumentException("Publisher with the following name already exists: " + publisher.getName());
+        }
+        publisher.setName(publisherUpdateDto.getName());
+
+        return PublisherResponseDto.getPublisherResponse(publisherRepository.save(publisher));
+    }
+
+
+    public void assignPublisherToBook(Book book, Long publisherId) {
+        Publisher publisher = publisherRepository.findById(publisherId)
+                .orElseThrow(() ->
+                        new PublisherNotFoundException("Publisher with the following id not found: " + publisherId));
+
+        publisher.addBook(book);
     }
 }
