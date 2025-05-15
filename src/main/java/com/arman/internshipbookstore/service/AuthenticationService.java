511260f9d7e4d050da13c2b9965bfeffb56e3ec5
Arman <melkonyan.arman.03@gmail.com>
2025-05-15 22:48:18 +0400

    Add refreshToken method

--- src/main/java/com/arman/internshipbookstore/service/AuthenticationService.java
+++ src/main/java/com/arman/internshipbookstore/service/AuthenticationService.java
@@ -2,13 +2,17 @@ package com.arman.internshipbookstore.service;
 
 import com.arman.internshipbookstore.security.dto.LoginRequestDto;
 import com.arman.internshipbookstore.security.dto.LoginResponse;
+import com.arman.internshipbookstore.security.dto.RefreshTokenDto;
 import com.arman.internshipbookstore.security.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +20,10 @@ public class AuthenticationService {
 
     private final AuthenticationManager authenticationManager;
     private final JwtUtil jwtUtil;
+    private final UserDetailsService userDetailsService;
 
     public LoginResponse authenticate(LoginRequestDto loginRequest) {
+
         final Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         loginRequest.getUsername(),
@@ -34,5 +40,27 @@ public class AuthenticationService {
                 .withAccessToken(accessToken)
                 .withRefreshToken(refreshToken)
                 .build();
+    }
+
+    public Optional<LoginResponse> refreshToken(RefreshTokenDto refreshTokenDto) {
+
+        String refreshToken = refreshTokenDto.getRefreshToken();
+
+        if (refreshToken == null || !jwtUtil.isVerified(refreshToken)) {
+            return Optional.empty();
+        }
+
+        String username = jwtUtil.getUsername(refreshToken);
+        UserDetails user = userDetailsService.loadUserByUsername(username);
+
+        String newAccessToken = jwtUtil.generateAccessToken(user);
+
+        LoginResponse resp = LoginResponse.builder()
+                .withUsername(user.getUsername())
+                .withAccessToken(newAccessToken)
+                .withRefreshToken(refreshToken)
+                .build();
+
+        return Optional.of(resp);
     }
 }
