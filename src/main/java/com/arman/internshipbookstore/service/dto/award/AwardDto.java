9ac8b4b10bb44dc265834f081e478bffa073ac2e
Arman <melkonyan.arman.03@gmail.com>
2025-05-16 03:36:36 +0400

    commit

--- src/main/java/com/arman/internshipbookstore/service/dto/award/AwardDto.java
+++ src/main/java/com/arman/internshipbookstore/service/dto/award/AwardDto.java
@@ -1,8 +1,6 @@
 package com.arman.internshipbookstore.service.dto.award;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Setter
