06d388646d4a5291a13502416abc9bce3a9dff22
Arman <melkonyan.arman.03@gmail.com>
2025-05-16 16:13:42 +0400

    Add GET method

--- src/main/java/com/arman/internshipbookstore/persistence/repository/AuthorRepository.java
+++ src/main/java/com/arman/internshipbookstore/persistence/repository/AuthorRepository.java
@@ -1,14 +1,28 @@
 package com.arman.internshipbookstore.persistence.repository;
 
 import com.arman.internshipbookstore.persistence.entity.Author;
+import com.arman.internshipbookstore.service.dto.author.AuthorResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
-    Author getAuthorByName(String name);
+    @Query("""
+            SELECT new com.arman.internshipbookstore.service.dto.author.AuthorResponseDto(
+                a.id,
+                a.name
+            )
+            FROM Author a
+            WHERE a.name LIKE CONCAT('%',:name,'%')
+""")
+    Page<AuthorResponseDto> getAuthorByName(String name, Pageable pageable);
 
     Author getAuthorById(Long id);
 
--- src/main/java/com/arman/internshipbookstore/persistence/repository/PublisherRepository.java
+++ src/main/java/com/arman/internshipbookstore/persistence/repository/PublisherRepository.java
@@ -1,14 +1,26 @@
 package com.arman.internshipbookstore.persistence.repository;
 
 import com.arman.internshipbookstore.persistence.entity.Publisher;
+import com.arman.internshipbookstore.service.dto.publisher.PublisherResponseDto;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PublisherRepository extends JpaRepository<Publisher, Long> {
 
-    Publisher getPublisherByName(String name);
+    @Query("""
+            SELECT new com.arman.internshipbookstore.service.dto.publisher.PublisherResponseDto(
+                p.id,
+                p.name
+            )
+            FROM Publisher p
+            WHERE p.name LIKE CONCAT('%',:name,'%')
+""")
+    Page<PublisherResponseDto> getPublisherByName(String name, Pageable pageable);
 
     boolean existsPublisherByName(@NotBlank String name);
 }
