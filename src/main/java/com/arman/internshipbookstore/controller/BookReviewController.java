27cb295af68e0216d132a7fd8659a3a438d8a0a3
Arman <melkonyan.arman.03@gmail.com>
2025-05-16 21:24:49 +0400

    Add CRUD in controller

--- src/main/java/com/arman/internshipbookstore/controller/BookReviewController.java
+++ src/main/java/com/arman/internshipbookstore/controller/BookReviewController.java
@@ -1,10 +1,45 @@
 package com.arman.internshipbookstore.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.arman.internshipbookstore.service.BookReviewService;
+import com.arman.internshipbookstore.service.dto.review.BookReviewCreateDto;
+import com.arman.internshipbookstore.service.dto.review.BookReviewResponseDto;
+import com.arman.internshipbookstore.service.dto.review.BookReviewUpdateDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/reviews")
+@RequiredArgsConstructor
 public class BookReviewController {
 
+    private final BookReviewService bookReviewService;
+
+    @PostMapping("/review")
+    public ResponseEntity<BookReviewResponseDto> addReview(@RequestBody @Valid BookReviewCreateDto bookReviewCreateDto,
+                                           Authentication authentication){
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(bookReviewService.addReview(bookReviewCreateDto,authentication));
+    }
+
+    @PostMapping("/{reviewId}")
+    public ResponseEntity<BookReviewResponseDto> editReview(@PathVariable("reviewId") Long reviewId,
+                                            @RequestBody @Valid BookReviewUpdateDto bookReviewUpdateDto,
+                                            Authentication authentication){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(bookReviewService.editReview(reviewId,bookReviewUpdateDto,authentication));
+    }
+
+    @DeleteMapping("/{bookId}/{reviewId}")
+    public ResponseEntity<Void> deleteReview(@PathVariable("bookId") Long bookId,
+                                             @PathVariable("reviewId") Long reviewId,
+                                             Authentication authentication){
+        bookReviewService.deleteReview(bookId,reviewId,authentication);
+        return ResponseEntity.noContent().build();
+    }
 }
