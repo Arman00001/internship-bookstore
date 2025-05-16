3729e1cf974792c3f855047da66a8b91e22ea7e8
Arman <melkonyan.arman.03@gmail.com>
2025-05-16 21:24:10 +0400

    Create dto

---
+++ src/main/java/com/arman/internshipbookstore/service/dto/review/BookReviewUpdateDto.java
@@ -0,0 +1,19 @@
+package com.arman.internshipbookstore.service.dto.review;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookReviewUpdateDto {
+    @NotNull
+    private Long bookId;
+
+    @NotBlank
+    private String review;
+
+    @NotNull
+    private Integer rating;
+}
