bc57026386cf019c1d60bda4b64b2ba85a184cba
Arman <melkonyan.arman.03@gmail.com>
2025-05-16 16:30:56 +0400

    Add GET method

--- src/main/java/com/arman/internshipbookstore/persistence/repository/AwardRepository.java
+++ src/main/java/com/arman/internshipbookstore/persistence/repository/AwardRepository.java
@@ -1,7 +1,10 @@
 package com.arman.internshipbookstore.persistence.repository;
 
 import com.arman.internshipbookstore.persistence.entity.Award;
+import com.arman.internshipbookstore.service.dto.award.AwardResponseDto;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -10,6 +13,16 @@ import java.util.Set;
 
 @Repository
 public interface AwardRepository extends JpaRepository<Award, Long> {
+    @Query("""
+            SELECT new com.arman.internshipbookstore.service.dto.award.AwardResponseDto(
+                aw.id,
+                aw.name
+            )
+            FROM Award aw
+            WHERE aw.name LIKE CONCAT('%',:name,'%')
+""")
+    Page<AwardResponseDto> findAwardsByName(String name, Pageable pageable);
+
     Award getAwardByName(String name);
 
     @Query("SELECT aw.name FROM Award aw")
